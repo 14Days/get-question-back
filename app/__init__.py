80bd12edc6f6ba9f8f5ceb4474733cffaba684ae
wangjq4214 <w1637894214@163.com>
2019-09-19 19:56:42 +0800

    添加跨域支持

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from app.config import FlaskConfig
 from app.controllers import register_routers
 from app.models import connect_db
@@ -6,6 +7,7 @@ from app.models import connect_db
 
 def new_flask_app() -> Flask:
     app = Flask(__name__)
+    CORS(app)
 
     # 添加配置文件
     app.config.from_object(FlaskConfig)
